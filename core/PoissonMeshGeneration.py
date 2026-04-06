f57ed8ee3fbd5d3741be4d2c3f02a0676d136ef8
youssef51-dotcom <yelrhabi@gmail.com>
2026-04-06 23:49:32 +0200

    Update PoissonMeshGeneration.py

--- core/PoissonMeshGeneration.py
+++ core/PoissonMeshGeneration.py
@@ -1,7 +1,6 @@
 import open3d as o3d
 import numpy as np
 from utils.meshUtils import smooth_point_cloud
-from utils.meshUtils import densify_pcd
 
 def poissonDepth():
     pcd = o3d.io.read_point_cloud("output/sparse_points.ply")
